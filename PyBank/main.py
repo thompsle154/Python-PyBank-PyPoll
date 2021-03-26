a8c340bb77367d45a4944893ab409d42f3aed95d
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-03-26 01:08:29 -0600

    Total Months and Amounts

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,34 +6,36 @@ import os
 import csv
 
 #Read the csv file
-budgetdata_csv = os.path.join('Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #Initializing lists to store data
 months = []
-profit_amt = [
+profit_amt = []
 #profit_change = []
 
-with open(budgetdata_csv, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+#Read csv file
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
     
     #Read the header row first
-    csv_header = next(csvreader)
+    header = next(csvreader)
     #print(f"Header: {csv_header}")
 
-    #Count total number of months
-    lines = len(list(csvreader))
-    print("Total Months : " + str(lines))
-    
     #Read the rows after the header and format amt as integer        
     #Get total of amounts in profit/loss column for all months
     
     for row in csvreader:
-        months.append([row[0], int(row[1])])
-        total_amt += (int(row[1]))
-        #date.append(row[0]+1)
-    #print ("Total : " + (total_amt))    
-    print(total_amt)
-    #print(alldata)
+        months.append(row[0])
+        profit_amt.append(int(row[1]))
+        #total_amt += (int(row[1]))
+
+        
+    #Loop through the amounts to get the monthly change
+      #  for r in range(len(profit_amt)-1:
+    
+    #print ("Total : " + (profit_amt))    
+    #print(profit_amt)
+    
 
     #Get Average Change
     #Find change between all months than get the average
@@ -50,9 +52,9 @@ with open(budgetdata_csv, newline='') as csvfile:
     #Create title header
 print("Financial Analysis")
 print("---------------------------") 
-     
-       #Get total number of months
-#print("Total Months: %d"%(csvreader.line_num))
+print(f"Total Months: {len(months)}")
+print(f"Total: ${sum(profit_amt)}")
+        
 
 
 
