46d11abd92690748fd54792b85b56163422c0cdf
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-03-26 01:55:00 -0600

    Update with average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,7 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 #Initializing lists to store data
 months = []
 profit_amt = []
-#profit_change = []
+profit_change = []
 
 #Read csv file
 with open(csvpath) as csvfile:
@@ -19,41 +19,39 @@ with open(csvpath) as csvfile:
     
     #Read the header row first
     header = next(csvreader)
-    #print(f"Header: {csv_header}")
-
-    #Read the rows after the header and format amt as integer        
-    #Get total of amounts in profit/loss column for all months
     
+    #Read the rows after the header and format amt as integer        
+    #Seperate out the columns
     for row in csvreader:
         months.append(row[0])
         profit_amt.append(int(row[1]))
-        #total_amt += (int(row[1]))
 
-        
     #Loop through the amounts to get the monthly change
-      #  for r in range(len(profit_amt)-1:
-    
-    #print ("Total : " + (profit_amt))    
-    #print(profit_amt)
-    
+    for r in range(len(profit_amt)-1):
+   
+        #Get the difference between the two months
+        profit_change.append(profit_amt[r+1] - profit_amt[r])
 
-    #Get Average Change
+#Look at min and max from amount change
+max_value = max(profit_change)
+min_value = min(profit_change)
+
+   #Get Average Change
     #Find change between all months than get the average
     #get max and min, then subtract, divided by number of lines (86)
     #input()
-    #    avg_change = 0
-    #for row in csv_reader:
-    #    First.append(row)
+       #    First.append(row)
     #    avg_change = (max(row[1]) - min(row[1]) / (lines)))
     #print("Average Change: ", str(avg_change))
 
        
-      
-    #Create title header
-print("Financial Analysis Test")
+#Put outside of loop - no indent     
+print("Financial Analysis")
 print("---------------------------") 
 print(f"Total Months: {len(months)}")
 print(f"Total: ${sum(profit_amt)}")
+print(f"Average Change: ${round((profit_change),2)}")
+
         
 
 
